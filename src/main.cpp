b44405edaa2d3c076a6b7f1dbc77cfa93dd4ec3f
Mykyta <69142225+Romanennko@users.noreply.github.com>
2024-11-08 23:02:29 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,31 @@
 int ledPins[] = {2, 4, 5, 13, 12};
 int numLeds = 5;
 
+int buttonPin = 18;
+int delayTime = 500;
+bool buttonState = false;
+
 void setup() {
     for (int i = 0; i < numLeds; i++) {
       pinMode(ledPins[i], OUTPUT);
     }
+    pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
     for (int i = 0; i < numLeds; i++) {
+      if (digitalRead(buttonPin) == LOW) {
+        delay(50);
+        if (!buttonState) {
+          delayTime = (delayTime == 500) ? 200 : 500;
+          buttonState = true;
+        }
+      } else {
+        buttonState = false;
+      }
+
       digitalWrite(ledPins[i], HIGH);
-      delay(300);
+      delay(delayTime);
       digitalWrite(ledPins[i], LOW);
     }
 }
